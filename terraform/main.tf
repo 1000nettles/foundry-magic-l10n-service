036e15c1902441fb604a3566e3fc141e6939bcf9
1000nettles <1000nettles@users.noreply.github.com>
2021-06-13 13:37:29 -0700

    Add boilerplate basic Lambda with API Gateway
    * Using https://learn.hashicorp.com/tutorials/terraform/lambda-api-gateway

---
+++ terraform/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "my-lambda1"
+  description   = "My awesome lambda function"
+  handler       = "main.handler"
+  runtime       = "nodejs14.x"
+  
+  lambda_role = aws_iam_role.lambda_exec.arn
+
+  source_path = "../example/main.js"
+
+  tags = {
+    Name = "my-lambda1"
+  }
+}
+
+ # IAM role which dictates what other AWS services the Lambda function
+ # may access.
+resource "aws_iam_role" "lambda_exec" {
+   name = "serverless_example_lambda"
+
+   assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_lambda_permission" "apigw" {
+   statement_id  = "AllowAPIGatewayInvoke"
+   action        = "lambda:InvokeFunction"
+   function_name = module.lambda_function.lambda_function_name
+   principal     = "apigateway.amazonaws.com"
+
+   # The "/*/*" portion grants access from any method on any resource
+   # within the API Gateway REST API.
+   source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+}
