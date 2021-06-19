e360932ed968e999d53c3ad8d0a756cb5ec81b2e
1000nettles <1000nettles@users.noreply.github.com>
2021-06-19 15:30:34 -0700

    Ensure /tmp file is deleted after

--- app/ModuleRetriever.js
+++ app/ModuleRetriever.js
@@ -6,9 +6,14 @@ const crypto = require("crypto");
 
 class ModuleRetriever {
 
+  constructor() {
+    this.tmpModuleName = crypto.randomBytes(20).toString('hex') + '.zip';
+  }
+
   async retrieve(manifestUrl) {
     const manifest = await this._getManifestPayload(manifestUrl);
     await this._downloadAndSave(manifest.download);
+    this._cleanup();
   }
 
   async _getManifestPayload(manifestUrl) {
@@ -26,25 +31,28 @@ class ModuleRetriever {
   }
 
   async _downloadAndSave(downloadUrl) {
-    const tmpModuleName = crypto.randomBytes(20).toString('hex') + '.zip';
 
     const response = await fetch(
       downloadUrl,
       { method: 'GET', timeout: 5000, size: 8388608 }
     );
 
-    const dest = fs.createWriteStream(`/tmp/${tmpModuleName}`);
+    const dest = fs.createWriteStream(`/tmp/${this.tmpModuleName}`);
     await response.body.pipe(dest);
 
     const { writeStream, promise } = this._uploadStream({
       Bucket: '1000nettles-foundry-magic-l18n-orig-modules',
-      Key: tmpModuleName,
+      Key: this.tmpModuleName,
     });
 
-    const readStream = fs.createReadStream(`/tmp/${tmpModuleName}`);
+    const readStream = fs.createReadStream(`/tmp/${this.tmpModuleName}`);
 
     readStream.pipe(writeStream);
     return promise;
+  }
+
+  _cleanup() {
+    fs.unlinkSync(`/tmp/${this.tmpModuleName}`);
   }
 
   /**
