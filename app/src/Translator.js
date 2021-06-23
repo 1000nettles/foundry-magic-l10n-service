1166fb1f340d1ad965c9fe89f7d712068168b8de
1000nettles <1000nettles@users.noreply.github.com>
2021-06-22 19:11:57 -0700

    Simplify and document Translator class

--- app/src/Translator.js
+++ app/src/Translator.js
@@ -2,44 +2,112 @@
 
 const AWS = require('aws-sdk');
 
+/**
+ * A class to execute the actual translation of strings.
+ */
 module.exports = class Translator {
 
-  async translate(translations, toTranslate) {
-    const enTranslation = translations.find(
-      translation => translation.lang === 'en'
-    );
-    console.log(enTranslation);
-
-    if (!enTranslation) {
-      console.error('No english translation found');
-      return;
-    }
-
+  constructor() {
     AWS.config.update({ region: 'us-east-1' });
-    const awsTranslate = new AWS.Translate();
+    this.awsTranslate = new AWS.Translate();
+  }
 
-    for (const locale of toTranslate) {
-      for (const entry of Object.entries(enTranslation.content)) {
-        const [key, value] = entry;
-        var params = {
-          SourceLanguageCode: 'en',
-          TargetLanguageCode: locale,
-          Text: value
-        };
+  /**
+   * Translate a list of Foundry translations to a list of target translations.
+   *
+   * @param {array} translations
+   *   A list of Foundry translation contents.
+   * @param {array} toTranslate
+   *   A list of language codes to translate too.
+   *
+   * @return {Promise<*[]>}
+   *   A promise containing the final translations.
+   */
+  async translate(translations, toTranslate) {
+    const baseTranslation = this._getBaseTranslation(translations);
+    let finalTranslations = [];
 
-        console.log(`About to translate ${value}`);
-  
-        await awsTranslate.translateText(params, (err, data) => {
-          if (err) {
-            console.log(err, err.stack); 
-          } else {
-            console.log(data['TranslatedText']);
-          }
-        }).promise();
+    for (const language of toTranslate) {
+      for (const entry of Object.entries(baseTranslation.content)) {
+        finalTranslations[language] = finalTranslations[language] || {};
+        const [stringId, text] = entry;
+        finalTranslations[language][stringId] = await this._getAWSTranslation(
+          baseTranslation.lang,
+          language,
+          text
+        );
 
         break;
       }
     }
+
+    console.log(finalTranslations);
+
+    return finalTranslations;
+  }
+
+  /**
+   * Get the base level translation to translate from.
+   *
+   * @param {array} translations
+   *   An array of translation objects.
+   *
+   * @return {object}
+   *   The base translation object.
+   *
+   * @private
+   */
+  _getBaseTranslation(translations) {
+    const baseLangCode = 'en';
+    const base = translations.find(
+      translation => translation.lang === baseLangCode
+    );
+
+    if (!base) {
+      throw new Error(
+        `Base language code ${baseLangCode} could not be found in the translations list`
+      );
+    }
+
+    return base;
+  }
+
+  /**
+   * Get the AWS Translate result for our target text.
+   * @param {string} source
+   *   The source language code. (The language to translate FROM.)
+   * @param {string} target
+   *   The target language code. (The language to translate TO.)
+   * @param {string} text
+   *   The text to translate.
+   *
+   * @return {Promise<string>}
+   *   A promise containing the final translated string.
+   *
+   * @private
+   */
+  async _getAWSTranslation(source, target, text) {
+    let finalTranslation;
+
+    const params = {
+      SourceLanguageCode: source,
+      TargetLanguageCode: target,
+      Text: text
+    };
+
+    console.log(`About to translate ${text}`);
+
+    await this.awsTranslate.translateText(params, (err, data) => {
+      if (err) {
+        throw new Error(
+          `Error translating "${params.Text}" to ${params.TargetLanguageCode}: ${err.code} - ${err.message}`
+        );
+      }
+
+      finalTranslation = data['TranslatedText'];
+    }).promise();
+
+    return finalTranslation;
   }
 
 }
